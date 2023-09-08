1761afa2486754b32f1d83f5e035e2e14b7c98ba
Baha Boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2023-09-08 19:05:13 +0200

    Initial commit

---
+++ lib/services/location.dart
@@ -0,0 +1,37 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+
+  late double latitude;
+  late double longitude;
+
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    permission = await Geolocator.requestPermission();
+
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+
+    latitude = position.latitude;
+    longitude = position.longitude;
+
+    return await Geolocator.getCurrentPosition();
+  }
+}
